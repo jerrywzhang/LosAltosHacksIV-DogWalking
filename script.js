10ca7c8a230ab111684bfa8d295c0d56bdd62a53
Rebecca Swernofsky <rswernofsky@gmail.com>
2019-03-23 11:51:24 -0700

    add initial javascript files

---
+++ script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Application Name! To be decided later</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+            crossorigin="anonymous"></script> <!-- jquery -->
+    <script type="text/javascript" src="script.js"></script> <!-- javascript -->
+</head>
+<body style="background: lightgrey">
+    <!-- <canvas id "canvas" style="border: 1px solid black; height: 300px; width: 300px"></canvas> -->
+
+</body>
+</html>
