d38697c7b6ae0074a380e02e73bfb2cf209b03e3
Rebecca Swernofsky <rswernofsky@gmail.com>
2019-03-23 16:02:46 -0700

    idek

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+const snapchatLogin = () => {
+    // redirect to the explore page
+    window.location.replace("/explore");
+};
+
+const googleLogin = () => {
+
+// redirect to the explore page
+    window.location.replace("/explore");
+};
